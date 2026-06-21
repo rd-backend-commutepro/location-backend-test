88baaff0fd5fc42c87975ad2c283920cf384573f
rd-backend-commutepro <rishabh.commutepro@gmail.com>
2026-06-21 18:20:46 +0530

    feat(admin): added admin.py

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Device, LocationLog
 
-# Register your models here.
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['device_id', 'name', 'created_at', 'location_count']
+    search_fields = ['device_id', 'name']
+    ordering = ['-created_at']
+
+    def location_count(self, obj):
+        return obj.logs.count()
+    location_count.short_description = 'Total Logs'
+
+
+@admin.register(LocationLog)
+class LocationLogAdmin(admin.ModelAdmin):
+    list_display = ['device', 'latitude', 'longitude',
+                    'speed', 'accuracy', 'timestamp', 'received_at']
+    list_filter = ['device']
+    search_fields = ['device__device_id', 'device__name']
+    ordering = ['-timestamp']
+    readonly_fields = ['received_at']
